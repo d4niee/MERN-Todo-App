a79aacf2583f518a101ff98f07a607151d141906
lambrech <daniel.lambrecht@student.reutlingen-university.de>
2023-04-11 14:25:08 +0200

    add themeProvider to app.tsx

--- todo-client/src/components/App.tsx
+++ todo-client/src/components/App.tsx
@@ -9,6 +9,7 @@ import Register from '../pages/Login/Register';
 import {ToastContainer} from 'react-toastify';
 import {User} from '../models/User';
 import AddingTodo from '../pages/AddingTodo/AddingTodo';
+import {ThemeProvider} from "../../provider/ThemeProvider";
 
 
 const App: FC = () => {
@@ -19,32 +20,34 @@ const App: FC = () => {
     }
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={'/'}>
-                    <Route index element={
-                        user && user._id ?
-                            <TodoList user={user} setLoginUser={setLoginUser}/> :
-                            <Login setLoginUser={setLoginUser}/>
-                    }/>
-                    <Route path={'/edit/:id'} element={<EditTodo/>}/>
-                    <Route path={'/add'} element={<AddingTodo/>}/>
-                    <Route path={'*'} element={<PageNotFound/>}/>
-                    <Route path='/Login' element={<Login setLoginUser={setLoginUser}/>}/>
-                    <Route path='/Register' element={<Register/>}/>
-                </Route>
-            </Routes>
-            <ToastContainer
-                position='bottom-center'
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                draggable
-                theme='colored'
-            />
-        </BrowserRouter>
+        <ThemeProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'}>
+                        <Route index element={
+                            user && user._id ?
+                                <TodoList user={user} setLoginUser={setLoginUser}/> :
+                                <Login setLoginUser={setLoginUser}/>
+                        }/>
+                        <Route path={'/edit/:id'} element={<EditTodo/>}/>
+                        <Route path={'/add'} element={<AddingTodo/>}/>
+                        <Route path={'*'} element={<PageNotFound/>}/>
+                        <Route path='/Login' element={<Login setLoginUser={setLoginUser}/>}/>
+                        <Route path='/Register' element={<Register/>}/>
+                    </Route>
+                </Routes>
+                <ToastContainer
+                    position='bottom-center'
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    draggable
+                    theme='colored'
+                />
+            </BrowserRouter>
+        </ThemeProvider>
     );
 };
 
